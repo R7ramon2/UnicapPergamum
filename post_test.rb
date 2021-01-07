011301f756552fe23eaa473225e75fc6cecbd1d2
R7ramon2 <ramonranieri@hotmail.com>
2021-01-07 13:38:57 -0300

    Update post_test.rb

--- post_test.rb
+++ post_test.rb
@@ -6,7 +6,7 @@ require 'socket'
 url = URI.parse("http://127.0.0.1:8000/") 
 http = Net::HTTP.new( url.host, url.port)
 request = Net::HTTP::Post.new( url.path )
-data = {"matricula" => "2015204740", "password" => "196722"}
+data = {"matricula" => "", "password" => ""}
 request.body = data.to_json
 binding.pry
 response = http.request( request )
